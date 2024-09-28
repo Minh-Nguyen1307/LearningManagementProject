d23676a76389d8d1c57b3f8b2c7965bc6a7a67f6
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2024-09-28 10:39:29 +0700

    .

--- LearningManagementProject/Components/Header/Header.jsx
+++ LearningManagementProject/Components/Header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 export default function Header() {
   return (
     <div>
-      
+      <div className='d-flex justify-content-around'>
+        <div className='header1'>
+        <Link to="/"><img src="./public/Byway.png" alt="logoByway" /></Link>
+        </div>
+        <div className='header2'>
+        <Link to ="">Categories</Link>
+        </div>
+        <div className='header3'>
+            <span><FontAwesomeIcon icon={faMagnifyingGlass} className='search-icon' /></span>
+            <input type="search" placeholder='Search courses' className='search-input' />
+        </div>
+        <div className='header4'>
+          Teach on Byway
+        </div>
+        <div className='header5'>
+            <span><FontAwesomeIcon icon={faCartShopping} className='cart-icon' /></span>
+            <button className='logIn'>Log in</button>
+            <button className='signIn'>Sign up</button>
+        </div>
+        </div>
     </div>
   )
 }
--- LearningManagementProject/src/App.jsx
+++ LearningManagementProject/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css'
+import HomePage from '../HomePage/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Router>
+      <Routes>
+        <Route path="/" element={
+            <>
+              <HomePage />
+            </>
+}/>
+      </Routes>
+    </Router>
     </>
   )
 }
