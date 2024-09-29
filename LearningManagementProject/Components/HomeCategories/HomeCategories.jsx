6479774b8f27a8bd949c93d3dfbc1ff7a8401873
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2024-09-29 15:52:43 +0700

    .

--- LearningManagementProject/Components/HomeCategories/HomeCategories.jsx
+++ LearningManagementProject/Components/HomeCategories/HomeCategories.jsx
@@ -1,36 +1,33 @@
 import React, { useState } from 'react'
 import ListCategories from './ListCategories/ListCategories'
 import { faMicroscope, faWindowMaximize, faCreditCard, faAtom } from '@fortawesome/free-solid-svg-icons';
-import { v4 as uuidv4 } from 'uuid';
 
-import { v4 as uuidv4 } from 'uuid';
 
-const uniqueId = uuidv4();
-console.log(uniqueId); // Prints a unique UUID like "fdd60ef4-ff1f-4c64-b84d-67e3ff2b3f08"
+
 
 
 
 const category = [
     {
-        id: uuidv4(),
+        id: 1,
         iconCategory: faMicroscope,
         nameCategory: "Astrology",
         numberCategory: "11 Courses",
     },
     {
-        id: uuidv4(),
+        id: 2,
         iconCategory: faWindowMaximize,
         nameCategory: "Development",
         numberCategory: "12 Courses",
     },
     {
-        id: uuidv4(),
+        id: 3,
         iconCategory: faCreditCard,
         nameCategory: "Marketing",
         numberCategory: "13 Courses",
     },
     {
-        id: uuidv4(),
+        id: 4,
         iconCategory: faAtom,
         nameCategory: "Physics",
         numberCategory: "14 Courses",
