1518147148af782eaf7184b04b5a88a7ef4c59ad
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2024-10-07 16:59:27 +0700

    .

--- LearningManagementProject/CoursesPage/CoursesPage.jsx
+++ LearningManagementProject/CoursesPage/CoursesPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../Components/Header/Header'
 import Footer from '../Components/Footer/Footer'
 import CoursesForm from '../Components/CoursesForm/CoursesForm'
-
+import './CoursesPage.css'
 export default function CoursesPage() {
   return (
     <div>
